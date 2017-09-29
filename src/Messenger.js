b6f47f60aaaec8c3a2475dc0259605bce9941952
Matt Johnson <Matthew.Johnson@transplace.com>
2017-09-29 13:09:04 -0400

    feat(Messenger): Add core ui components

--- src/Messenger.js
+++ src/Messenger.js
@@ -1,3 +1,42 @@
+const Header = () => `
+  <header>
+    <h4>Messenger</h4>
+  </header>
+`;
+
+const InputArea = () => `
+  <form>
+    <input>
+    <button>&gt;</button>
+  </form>
+`;
+
 export default class Messenger {
+  constructor(options) {
+    this.element = options.element;
+    this.header = this.createHeader();
+    this.header.innerHTML = Header();
+    this.messageListArea = this.createMessageListArea();
+    this.inputArea = this.createInputArea();
+    this.inputArea.innerHTML = InputArea();
+  }
+
+  createHeader() {
+    const header = document.createElement('div');
+    this.element.appendChild(header);
+    return header;
+  }
+
+  createMessageListArea() {
+    const messageListArea = document.createElement('div');
+    this.element.appendChild(messageListArea);
+    return messageListArea;
+  }
+
+  createInputArea() {
+    const inputArea = document.createElement('div');
+    this.element.appendChild(inputArea);
+    return inputArea;
+  }
 }
 
