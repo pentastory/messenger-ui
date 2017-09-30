96f20be4c67b9baece5202ee82c86916b41ab5a0
Matt Johnson <Matthew.Johnson@transplace.com>
2017-09-29 20:18:17 -0400

    feat(Message): Create message on form submit

--- src/Messenger.ts
+++ src/Messenger.ts
@@ -1,16 +1,6 @@
-const Header = () => `
-  <header>
-    <h4>Messenger</h4>
-  </header>
-`;
-
-const InputArea = () => `
-  <form>
-    <input>
-    <button>&gt;</button>
-  </form>
-`;
-
+import Header from './components/Header';
+import InputArea from './components/InputArea';
+import Message from './components/Message';
 export default class Messenger {
   private element: Element;
   private header: Element;
@@ -23,7 +13,6 @@ export default class Messenger {
     this.header.innerHTML = Header();
     this.messageListArea = this.createMessageListArea();
     this.inputArea = this.createInputArea();
-    this.inputArea.innerHTML = InputArea();
   }
 
   createHeader() {
@@ -40,7 +29,31 @@ export default class Messenger {
 
   createInputArea() {
     const inputArea = document.createElement('div');
+    inputArea.innerHTML = InputArea();
     this.element.appendChild(inputArea);
+
+    // Add submit event listener
+    const form = inputArea.querySelector('form');
+    form.addEventListener('submit', event => this.onSubmit(event));
+
     return inputArea;
+  }
+
+  addMessage(data) {
+    const message = document.createElement('div');
+    message.innerHTML = Message(data);
+    this.messageListArea.appendChild(message);
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+
+    const input = this.inputArea.querySelector('input');
+    const text = input.value;
+    input.value = '';
+
+    if (text === '') return;
+
+    this.addMessage({ author: '', content: text, date: Date(), isMe: true });
   }
 }
---
+++ src/components/Message.ts
@@ -0,0 +1,7 @@
+const Message = ({ author, content, date, isMe }) => `
+  <div>
+    ${content}
+  </div>
+`
+
+export default Message;
